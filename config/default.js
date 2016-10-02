2a8f9cbd24c5eb901073c881b2ec4c1a9c4e53d5
Sergiu Nacu <sergiuna@microsoft.com>
2016-10-02 21:55:38 +0200

    add configuration per environment

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  app: {
+    port: 3000,
+    host: 'localhost',
+    autoStart: false
+  },
+  build: {
+    dir: 'dist/client'
+  }
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const compression = require('compression');
+const express = require('express');
+const path = require('path');
+const config = require('config');
+const port = config.get('app.port');
+const outDir = config.get('build.dir');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(outDir));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, `${outDir}/index.html`));
+});
+
+const server = app.listen(port, () => {
+  let address = server.address();
+  console.log(`Listening on port: ${address.port}`);
+  let autoStart = config.get('app.autoStart');
+  console.log(config.get('app'));
+  if (autoStart) {
+    require('open')(`http://localhost:${address.port}`);
+  }
+});
